6cc24789f0b5426e13a8f8b558a6025b908b1469
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-03-16 22:46:28 +0330

    update Add PathExtentions

---
+++ Art.Gallery.Common/PathExtentions.cs
@@ -0,0 +1,7 @@
+﻿namespace Art.Gallery.Common;
+public class PathExtentions
+{
+
+    public static string DomainAddress = "https://localhost:44350";
+
+}
--- Art.Gallery.Web/Controllers/ProductsController.cs
+++ Art.Gallery.Web/Controllers/ProductsController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Art.Gallery.Common;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 namespace Art.Gallery.Web.Controllers;
+/// <summary>
+/// www.technoto.org/api/Products/GetProducts
+/// https://localhost:44350/api/Products/GetProducts
+/// </summary>
 [Route("api/[controller]")]
 [ApiController]
 public class ProductsController : ControllerBase
@@ -14,7 +19,7 @@ public class ProductsController : ControllerBase
             new { Id = 2, Name = "Product 2", Price = 200 },
             new { Id = 3, Name = "Product 3", Price = 300 }
         };
-
+       
         return Ok(products);
     }
 }
