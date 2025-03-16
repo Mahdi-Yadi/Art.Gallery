8a11dcd00f6eb309a53ea828dab61f94cd22be31
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-03-16 22:22:30 +0330

    update project

--- Art.Gallery.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Art.Gallery.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
