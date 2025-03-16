337caa38965feff5e0103f72f89d8e604ba24071
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-03-16 22:24:54 +0330

    add products api

---
+++ Art.Gallery.Web/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace Art.Gallery.Web.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        var products = new List<object>
+        {
+            new { Id = 1, Name = "Product 1", Price = 100 },
+            new { Id = 2, Name = "Product 2", Price = 200 },
+            new { Id = 3, Name = "Product 3", Price = 300 }
+        };
+
+        return Ok(products);
+    }
+}
