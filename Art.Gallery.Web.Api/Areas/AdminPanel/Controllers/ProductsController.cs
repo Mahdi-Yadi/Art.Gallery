926267155d22ef0b63293702891f4d4583a30a53
Mahdi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-04-10 20:37:56 +0330

    a

--- Art.Gallery.Web.Api/Areas/AdminPanel/Controllers/ProductsController.cs
+++ Art.Gallery.Web.Api/Areas/AdminPanel/Controllers/ProductsController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 namespace Art.Gallery.Web.Api.Areas.AdminPanel.Controllers;
@@ -8,4 +8,27 @@ namespace Art.Gallery.Web.Api.Areas.AdminPanel.Controllers;
 //[Authorize]
 public class ProductsController : ControllerBase
 {
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
 }
