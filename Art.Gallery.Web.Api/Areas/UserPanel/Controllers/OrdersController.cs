238becd301b7a181a6eee078c723c1b49ee5db54
Mahdi-Yadi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-06-05 12:36:08 +0330

    comment payment

--- Art.Gallery.Web.Api/Areas/UserPanel/Controllers/OrdersController.cs
+++ Art.Gallery.Web.Api/Areas/UserPanel/Controllers/OrdersController.cs
@@ -162,117 +162,125 @@ public class OrdersController : ControllerBase
                 });
             }
 
-        }
-        catch (Exception)
-        {
-            return BadRequest();
-        }
-
-        return BadRequest();
-    }
-
-    [HttpGet("Pay/{userId}")]
-    public async Task<IActionResult> Pay(string userId)
-    {
-        var order = _orderService.GetOpenOrder(userId);
-
-        if (order == null || order.OrderDetailsDto.Count == 0)
-            return BadRequest();
-
-        decimal totalPrice = 0;
-
-        foreach (var item in order.OrderDetailsDto)
-        {
-            decimal price = (decimal)item.Price * item.Count;
-
-            totalPrice += price;
-        }
-
-        try
-        {
-            string trackingCode = $"{DateTime.Now:yyyyMMddHHmmmmssffff}";
-
-            string callback = "https://localhost:44331/VerifyPay";
-
-            totalPrice *= 10;
-
-            var res = await _onlinePayment.RequestAsync(invoice =>
-            {
-                invoice.SetZarinPalData("خرید از وب سایت گالری هنر")
-                    .SetTrackingNumber(Convert.ToInt64(trackingCode))
-                    .SetAmount(totalPrice)
-                    .SetCallbackUrl(callback)
-                    .SetGateway("ZarinPal");
-            });
-
-            var resOrder = _orderService.UpdateOrderForPayment(order.OrderId, trackingCode);
-            if (!resOrder)
-            {
-                resOrder = _orderService.UpdateOrderForPayment(order.OrderId, trackingCode);
-                if (!resOrder)
-                    return BadRequest();
-            }
-
-            if (res.IsSucceed)
-                return res.GatewayTransporter.TransportToGateway();
-
-        }
-        catch (Exception)
-        {
-            return BadRequest();
-        }
-
-        return BadRequest();
-    }
-
-    [HttpGet("VerifyPay")]
-    [HttpPost("VerifyPay")]
-    public async Task<IActionResult> VerifyPay()
-    {
-        var invoice = await _onlinePayment.FetchAsync();
-
-        if (invoice == null)
-        {
-            return BadRequest();
-        }
-
-        if (invoice.Status != PaymentFetchResultStatus.ReadyForVerifying)
-        {
-            return BadRequest();
-        }
-
-        var verifyResult = await _onlinePayment.VerifyAsync(invoice);
-
-        if (verifyResult.Status == PaymentVerifyResultStatus.Succeed)
-        {
-            var res = _orderService.UpdateOrderAfterPayment(verifyResult.TrackingNumber.ToString(), verifyResult.TransactionCode);
-
-            if (res)
-            {
-                return Ok(new
-                {
-                    status = "Success",
-                    code = verifyResult.TransactionCode
-                });
-            }
-            else
-            {
-                return Ok(new
-                {
-                    status = "Success",
-                    code = "پرداخت انجام شد، اما خطای سیستم داریم.!!!"
-                });
-            }
-        }
-        else
-        {
             return Ok(new
             {
                 status = "Error",
-                code = "پرداخت انجام نشد!"
+                code = verifyResult
             });
+        }
+        catch (Exception)
+        {
+            return BadRequest();
         }
 
     }
+
+    #region Pay 
+
+    //[HttpGet("Pay/{userId}")]
+    //public async Task<IActionResult> Pay(string userId)
+    //{
+    //    var order = _orderService.GetOpenOrder(userId);
+
+    //    if (order == null || order.OrderDetailsDto.Count == 0)
+    //        return BadRequest();
+
+    //    decimal totalPrice = 0;
+
+    //    foreach (var item in order.OrderDetailsDto)
+    //    {
+    //        decimal price = (decimal)item.Price * item.Count;
+
+    //        totalPrice += price;
+    //    }
+
+    //    try
+    //    {
+    //        string trackingCode = $"{DateTime.Now:yyyyMMddHHmmmmssffff}";
+
+    //        string callback = "https://localhost:44331/VerifyPay";
+
+    //        totalPrice *= 10;
+
+    //        var res = await _onlinePayment.RequestAsync(invoice =>
+    //        {
+    //            invoice.SetZarinPalData("خرید از وب سایت گالری هنر")
+    //                .SetTrackingNumber(Convert.ToInt64(trackingCode))
+    //                .SetAmount(totalPrice)
+    //                .SetCallbackUrl(callback)
+    //                .SetGateway("ZarinPal");
+    //        });
+
+    //        var resOrder = _orderService.UpdateOrderForPayment(order.OrderId, trackingCode);
+    //        if (!resOrder)
+    //        {
+    //            resOrder = _orderService.UpdateOrderForPayment(order.OrderId, trackingCode);
+    //            if (!resOrder)
+    //                return BadRequest();
+    //        }
+
+    //        if (res.IsSucceed)
+    //            return res.GatewayTransporter.TransportToGateway();
+
+    //    }
+    //    catch (Exception)
+    //    {
+    //        return BadRequest();
+    //    }
+
+    //    return BadRequest();
+    //}
+
+    //[HttpGet("VerifyPay")]
+    //[HttpPost("VerifyPay")]
+    //public async Task<IActionResult> VerifyPay()
+    //{
+    //    var invoice = await _onlinePayment.FetchAsync();
+
+    //    if (invoice == null)
+    //    {
+    //        return BadRequest();
+    //    }
+
+    //    if (invoice.Status != PaymentFetchResultStatus.ReadyForVerifying)
+    //    {
+    //        return BadRequest();
+    //    }
+
+    //    var verifyResult = await _onlinePayment.VerifyAsync(invoice);
+
+    //    if (verifyResult.Status == PaymentVerifyResultStatus.Succeed)
+    //    {
+    //        var res = _orderService.UpdateOrderAfterPayment(verifyResult.TrackingNumber.ToString(), verifyResult.TransactionCode);
+
+    //        if (res)
+    //        {
+    //            return Ok(new
+    //            {
+    //                status = "Success",
+    //                code = verifyResult.TransactionCode
+    //            });
+    //        }
+    //        else
+    //        {
+    //            return Ok(new
+    //            {
+    //                status = "Success",
+    //                code = "پرداخت انجام شد، اما خطای سیستم داریم.!!!"
+    //            });
+    //        }
+    //    }
+    //    else
+    //    {
+    //        return Ok(new
+    //        {
+    //            status = "Error",
+    //            code = "پرداخت انجام نشد!"
+    //        });
+    //    }
+
+    //}
+
+    #endregion
 
 }
