c629c3d927c643531c08d648dacfc054aa1bcf52
Mahdi-Yadi <137503197+Mahdi-Yadi@users.noreply.github.com>
2025-06-05 21:42:58 +0330

    a

--- Art.Gallery.Emails/EmailService.cs
+++ Art.Gallery.Emails/EmailService.cs
@@ -8,17 +8,17 @@ public class EmailService
         var result = false;
         try
         {
-            MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("mail.technoto.org");
-            mail.From = new MailAddress("gallery@technoto.org", "گالری Technoto");
-            mail.To.Add(EmailAddress);
-            mail.Subject = Subject;
-            mail.Body = Body;
-            mail.IsBodyHtml = true;
-            SmtpServer.Port = 587;
-            SmtpServer.EnableSsl = true;
-            SmtpServer.Credentials = new NetworkCredential("gallery@technoto.org", "tfbdmas#4mcs3424rcxCLDLD");
-            SmtpServer.Send(mail);
+            //MailMessage mail = new MailMessage();
+            //SmtpClient SmtpServer = new SmtpClient("mail.technoto.org");
+            //mail.From = new MailAddress("gallery@technoto.org", "گالری Technoto");
+            //mail.To.Add(EmailAddress);
+            //mail.Subject = Subject;
+            //mail.Body = Body;
+            //mail.IsBodyHtml = true;
+            //SmtpServer.Port = 587;
+            //SmtpServer.EnableSsl = true;
+            //SmtpServer.Credentials = new NetworkCredential("gallery@technoto.org", "tfbdmas#4mcs3424rcxCLDLD");
+            //SmtpServer.Send(mail);
             result = true;
         }
         catch (Exception)
